81787329d99ac6715cd8b5277cac4e6f46a59a29
jereycui2008 <jeremycuid@gmail.com>
2023-04-15 11:30:46 -0700

    4/15/23 Today, I did algorithms 1 prime or not.

---
+++ src/_01_algorithms/_1_prime_or_not/Primeornot.java
@@ -0,0 +1,11 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Primeornot {
+public static void main(String[] args) {
+	String number = JOptionPane.showInputDialog("Please enter a number");
+	int numbers = Integer.parseInt(number);
+	
+}
+}
