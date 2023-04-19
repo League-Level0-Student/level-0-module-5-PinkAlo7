8bd39f7d519888cd49def8dc96d4bde7198a62cf
league <ameeralih786@gmail.com>
2023-04-19 16:59:46 -0700

    4/19/23 Today, I did algorithms 1 prime or not, and algorithms 2 fibonacci in module 5.

--- src/_01_algorithms/_1_prime_or_not/Primeornot.java
+++ src/_01_algorithms/_1_prime_or_not/Primeornot.java
@@ -3,9 +3,17 @@ package _01_algorithms._1_prime_or_not;
 import javax.swing.JOptionPane;
 
 public class Primeornot {
-public static void main(String[] args) {
-	String number = JOptionPane.showInputDialog("Please enter a number");
-	int numbers = Integer.parseInt(number);
-	
-}
-}
+	public static void main(String[] args) {
+		String number = JOptionPane.showInputDialog("Please enter a number");
+		int numbers = Integer.parseInt(number);
+		for (int i = 2; i < numbers; i++) {
+			if (numbers % i == 0) {
+				JOptionPane.showMessageDialog(null, "Your number is not a prime number");
+				System.exit(0);
+			} 
+
+			}
+    JOptionPane.showMessageDialog(null,"Your number is prime!");
+		}
+	}
+
