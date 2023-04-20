4a910c40df69f03873be1bc20fb58b75e63716e0
league <ameeralih786@gmail.com>
2023-04-19 17:01:44 -0700

    4/19/23 Today, I did algorithms 1 prime or not, and algorithms 2 fibonacci.

---
+++ src/_01_algorithms/_2_fibonacci/Fibonacci.java
@@ -0,0 +1,16 @@
+package _01_algorithms._2_fibonacci;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		
+	
+int number1 = 0;
+int number2 = 1;
+int number3 = number1 + number2;
+for(int i=1; i<=10;i++) {
+System.out.println(number3);
+}
+
+
+}
+}
